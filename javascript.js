98ffd615cdf2f7d22251d6008025b1769affc73b
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2020-10-23 21:42:43 -0600

    getting the current weather data to display.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 
   // done make api call
   // get the array 
@@ -10,7 +12,7 @@
 function currentWeather(response){
   var lat = response.city.coord.lat
   var lon = response.city.coord.lon
-  var weather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon + "&exclude=minutely,hourly&appid=ce4b2cc1bf796919d588834409f184a8"
+  var weather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=ce4b2cc1bf796919d588834409f184a8"
 
   $.ajax({
     url: weather,
@@ -18,18 +20,33 @@ function currentWeather(response){
   })
 .then(function(response) {
 
+  var temp = response.current.temp
+  var humid = response.current.humidity
+  var wind = response.current.wind_speed
+  var uv = response.current.uvi
+ 
+  $("#temp").append("Tempurature: ", temp) 
+  $("#humidity").append("Humidity: ", humid + "%")
+  $("#wind").append("Wind Speed: ", wind)
+  $("#uvIndex").append("UV Index: ", uv)
+ if(uv < 3){
+   $("#uvIndex").addClass("p-3 mb-2 bg-success text-white")
+ } else{
+  $("#uvIndex").addClass("p-3 mb-2 bg-danger text-white")
+ }
 
-  $("#weatherBox").text(JSON.stringify(response.daily))
+
   console.log(response)
 })
-}
+};
 
 
 $(".btn").on("click", function(e) {
   e.preventDefault();
   searchData = $("#search").val();
-  searchingCity(searchData);
 
+  searchingCity(searchData);
+  $(".clear").empty();
   console.log(searchData)
   });
    
@@ -43,14 +60,22 @@ function searchingCity(searchData){
       })
        
         .then(function(response) {
-          console.log(response)
+
+      var location = response.city.name
+     
+      $("#city").append(location)
+      
+      
+
+
+          //console.log(location)
           currentWeather(response);
          
           });
         
          }
 
-        
+        });  
 
      
 
